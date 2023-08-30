49429170841f410ccc439f7507e2f4c1a16ac5b4
Loganphx <loganphx1@gmail.com>
2023-08-30 14:19:53 -0500

    - Created ExceptionMiddleware for API

--- API/DTOs/RegisterRequest.cs
+++ API/DTOs/RegisterRequest.cs
@@ -5,8 +5,10 @@ namespace API.DTOs;
 public class RegisterRequest
 {
     [Required]
+    [StringLength(32, MinimumLength = 5)]
     public string Username { get; set; }
     
     [Required]
+    [StringLength(32, MinimumLength = 8)]
     public string  Password { get; set; }
 }
--- API/Program.cs
+++ API/Program.cs
@@ -1,4 +1,5 @@
 using API.Extensions;
+using API.Middleware;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,18 +9,24 @@ builder.Services.AddControllers();
 builder.Services.AddApplicationServices(builder.Configuration);
 builder.Services.AddIdentityServices(builder.Configuration);
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-// builder.Services.AddEndpointsApiExplorer();
-// builder.Services.AddSwaggerGen();
+builder.Services.AddEndpointsApiExplorer();
+//builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
+app.UseMiddleware<ExceptionMiddleware>();
 // Configure the HTTP request pipeline.
 // if (app.Environment.IsDevelopment())
 // {
-//     app.UseSwagger();
-//     app.UseSwaggerUI();
+     //app.UseDeveloperExceptionPage();
+     //app.UseSwagger();
+     //app.UseSwaggerUI();
 // }
-app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
+app.UseCors(builder => builder
+     .AllowAnyHeader()
+     .AllowAnyMethod()
+     .WithOrigins("https://localhost:4200"));
+
 //app.UseHttpsRedirection();
 app.UseAuthentication();
 app.UseAuthorization();
