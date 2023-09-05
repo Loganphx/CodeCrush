dd7bd6482fb7b0bd437c2cddb39bd08f8a33ed86
Loganphx <loganphx1@gmail.com>
2023-09-05 12:13:39 -0500

    - Created Photo Upload Mechanics

---
+++ API/DTOs/PhotoDto.cs
@@ -0,0 +1,17 @@
+﻿using API.Entities;
+
+namespace API.DTOs;
+
+public class PhotoDto
+{
+    public int    Id     { get; set; }
+    public string Url    { get; set; }
+    public bool   IsMain { get; set; }
+
+    public PhotoDto(Photo photo)
+    {
+        Id     = photo.Id;
+        Url    = photo.Url;
+        IsMain = photo.IsMain;
+    }
+}
