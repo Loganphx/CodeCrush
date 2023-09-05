b2cbf8681c818d57bf22bbef9059335bef70ba90
Loganphx <loganphx1@gmail.com>
2023-09-05 12:13:55 -0500

    - Updated Register Form

--- API/DTOs/RegisterRequest.cs
+++ API/DTOs/RegisterRequest.cs
@@ -5,10 +5,29 @@ namespace API.DTOs;
 public class RegisterRequest
 {
     [Required]
-    [StringLength(32, MinimumLength = 5)]
+    [StringLength(32, MinimumLength = 4)]
     public string Username { get; set; }
     
     [Required]
-    [StringLength(32, MinimumLength = 8)]
+    [EmailAddress]
+    public string Email { get; set; }
+
+    [Required]
+    public string KnownAs { get; set; }
+
+    [Required]
+    public string Gender { get; set; }
+    
+    [Required]
+    public DateOnly DateOfBirth { get; set; }
+    
+    [Required]
+    public string City { get; set; }
+
+    [Required]
+    public string Country { get; set; }
+
+    [Required]
+    [StringLength(64, MinimumLength = 8)]
     public string  Password { get; set; }
 }
--- client/src/app/register/register.component.ts
+++ client/src/app/register/register.component.ts
@@ -1,53 +1,117 @@
-import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
-import {FormControl, FormGroup, Validators} from "@angular/forms";
+import {Component, EventEmitter, OnDestroy, OnInit, Output} from '@angular/core';
+import {AbstractControl, FormBuilder, FormControl, FormGroup, ValidatorFn, Validators} from "@angular/forms";
 import {AccountService} from "../_services/account.service";
 import {ToastrService} from "ngx-toastr";
+import {Subject, takeUntil} from "rxjs";
+import {Router} from "@angular/router";
 
 export interface RegisterForm
 {
   username: FormControl<string>;
-  email: FormControl<string>
+  email: FormControl<string>;
+  knownAs: FormControl<string>;
+  gender: FormControl<string>;
+  dateOfBirth: FormControl<string>;
+  city: FormControl<string>;
+  country: FormControl<string>;
   password: FormControl<string>;
+  confirmPassword: FormControl<string>;
+}
+
+export function matchValuesValidator(matchTo: string): ValidatorFn {
+  return (control: AbstractControl) => {
+    if(!control || !control.parent) return null;
+    const matchControl = control.parent.get(matchTo)?.value;
+    return control.value === matchControl ? null : {notMatching: true}
+  }
 }
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.scss']
 })
-export class RegisterComponent implements OnInit{
+export class RegisterComponent implements OnInit, OnDestroy{
+  private _destroyed$: Subject<boolean> = new Subject<boolean>();
+
+  public registerFormGroup: FormGroup<RegisterForm>;
+  public maxDate = new Date();
+  public validationErrors: string[] | undefined;
+
   //@Input() usersFromHomeComponent: any;
   @Output() cancelRegister = new EventEmitter()
-  model: any = {};
 
-  registerFormGroup = new FormGroup<RegisterForm>(
-    {
-    username: new FormControl('', {nonNullable:true,
-      validators: [Validators.required, Validators.minLength(6)]}),
-    email: new FormControl('', {nonNullable:true,
-      validators: [Validators.required,   Validators.email]}),
-    password: new FormControl('', {nonNullable:true,
-      validators: [Validators.required, Validators.minLength(8)]}),
-  });
 
-  constructor(private accountService: AccountService, private toastr: ToastrService) {
+  constructor(private accountService: AccountService, private toastr: ToastrService,
+              private formBuilder: FormBuilder, private router: Router) {
+    this.registerFormGroup = this.formBuilder.group<RegisterForm>(
+      {
+        username: new FormControl('', {nonNullable:true,
+          validators: [Validators.required]}),
+        email: new FormControl('', {nonNullable:true,
+          validators: [Validators.required,   Validators.email]}),
+        knownAs: new FormControl('', {nonNullable:true,
+          validators: [Validators.required, Validators.maxLength(64)]}),
+        gender: new FormControl('', {nonNullable:true,
+          validators: [Validators.required, Validators.maxLength(64)]}),
+        dateOfBirth: new FormControl('', {nonNullable:true,
+          validators: [Validators.required, Validators.maxLength(64)]}),
+        city: new FormControl('', {nonNullable:true,
+          validators: [Validators.required, Validators.maxLength(64)]}),
+        country: new FormControl('', {nonNullable:true,
+          validators: [Validators.required, Validators.maxLength(64)]}),
+        password: new FormControl('', {nonNullable:true,
+          validators: [Validators.required, Validators.minLength(8), Validators.maxLength(64)]}),
+        confirmPassword: new FormControl('', {nonNullable:true, validators: [Validators.required, matchValuesValidator('password')]})
+      });
   }
 
   ngOnInit(): void {
+    this.registerFormGroup.controls.password.valueChanges
+      .pipe(
+        takeUntil(this._destroyed$),
+        //tap(value => console.log("Tap", value))
+      )
+      .subscribe({
+        next: () => this.registerFormGroup.controls.confirmPassword.updateValueAndValidity()
+      })
+    this.maxDate.setFullYear(this.maxDate.getFullYear()-18)
+  }
+
+  ngOnDestroy(): void {
+    this._destroyed$.next(true)
   }
 
   register()
   {
-    const username = this.registerFormGroup.controls.username.getRawValue();
-    const email = this.registerFormGroup.controls.email.getRawValue();
-    const password = this.registerFormGroup.controls.password.getRawValue();
+    const username: string = this.registerFormGroup.controls.username.getRawValue();
+    const email: string = this.registerFormGroup.controls.email.getRawValue();
+    const knownAs: string = this.registerFormGroup.controls.knownAs.getRawValue();
+    const gender: string = this.registerFormGroup.controls.gender.getRawValue();
+    const dateOfBirth = this.getDateOnly(this.registerFormGroup.controls.dateOfBirth.getRawValue());
+    const city: string = this.registerFormGroup.controls.city.getRawValue();
+    const country: string = this.registerFormGroup.controls.country.getRawValue();
+    const password: string = this.registerFormGroup.controls.password.getRawValue();
 
-    this.accountService.register({"username":username, "email":email, "password":password}).subscribe({
+    const values = {...this.registerFormGroup.value, dateOfBirth:dateOfBirth};
+    const model = {
+      "username":username,
+      "email":email,
+      "knownAs":knownAs,
+      "gender":gender,
+      "dateOfBirth":dateOfBirth,
+      "city":city,
+      "country":country,
+      "password":password
+    };
+
+    this.accountService.register(values).subscribe({
       next: () => {
-        this.cancel();
+        this.router.navigateByUrl('/members');
       },
       error: error => {
         console.log(error)
         this.toastr.error(error.error.errors)
+        this.validationErrors = error;
       }
     })
   }
@@ -57,4 +121,10 @@ export class RegisterComponent implements OnInit{
     this.cancelRegister.emit(false)
   }
 
+  private getDateOnly(dob: string | undefined) {
+    if(!dob) return;
+    let theDob = new Date(dob);
+    return new Date(theDob.setMinutes(theDob.getMinutes()-theDob.getTimezoneOffset()))
+      .toISOString().slice(0,10)
+  }
 }
