05bf0213d6170b1d904b082c9ea433d57c09a75d
Logan Ingram <loganphx1@gmail.com>
2024-06-08 19:54:12 -0500

    - Created Admin Service to get Users with Roles from API - Updated User Management Component to use Admin Service to display user role data.

--- client/src/app/admin/user-management/user-management.component.ts
+++ client/src/app/admin/user-management/user-management.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {AdminService} from "../../_services/admin.service";
+import {User} from "../../_models/user";
 
 @Component({
   selector: 'app-user-management',
   templateUrl: './user-management.component.html',
   styleUrls: ['./user-management.component.scss']
 })
-export class UserManagementComponent {
+export class UserManagementComponent implements OnInit {
+  users: User[] = [];
+
+  constructor(private adminService: AdminService) {
+  }
+
+  ngOnInit(): void {
+    this.getUsersWithRoles();
+  }
+
+  getUsersWithRoles() {
+    this.adminService.getUserWithRoles().subscribe({
+      next: users => this.users = users
+    })
+  }
 
 }
