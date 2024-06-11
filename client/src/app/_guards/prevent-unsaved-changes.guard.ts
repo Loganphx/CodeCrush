814c8fdec45f7f8fcea1a17a33fe1f8101ecee36
Logan Ingram <loganphx1@gmail.com>
2024-06-11 02:01:02 -0500

    - Replaced old Unsaved Changes Prompt with a Confirm Dialog Modal.

--- client/src/app/_guards/prevent-unsaved-changes.guard.ts
+++ client/src/app/_guards/prevent-unsaved-changes.guard.ts
@@ -1,9 +1,14 @@
-import { CanDeactivateFn } from '@angular/router';
+import {CanDeactivateFn} from '@angular/router';
 import {MemberEditComponent} from "../members/member-edit/member-edit.component";
+import {ConfirmService} from "../_services/confirm.service";
+import {inject} from "@angular/core";
 
 export const preventUnsavedChangesGuard: CanDeactivateFn<MemberEditComponent> = (component) => {
-  if(component.editMemberFormGroup.dirty){
-    return confirm("Are you sure you want to continue? Any unsaved changes will be lost")
+  const confirmService = inject(ConfirmService);
+  if (component.editMemberFormGroup.dirty) {
+    // return confirm("Are you sure you want to continue? Any unsaved changes will be lost")
+
+    return confirmService.confirm();
   }
   return true;
 };
