78b68918c61172d2e084a76a5e9d862b0ef67b5e
Logan Ingram <loganphx1@gmail.com>
2024-06-12 16:23:37 -0500

    - Added Github Actions Workflow

--- API/Program.cs
+++ API/Program.cs
@@ -15,11 +15,6 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
-
-builder.Services.AddDbContext<DataContext>(opt =>
-{
-     opt.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"));
-});
 builder.Services.AddCors();
 builder.Services.AddIdentityServices(builder.Configuration);
 builder.Services.AddScoped<ITokenService, TokenService>();
@@ -38,22 +33,47 @@ builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+var connString = "";
+if (builder.Environment.IsDevelopment())
+{
+    connString = builder.Configuration.GetConnectionString("DefaultConnection");
+}
+else
+{
+    var connUrl = Environment.GetEnvironmentVariable("DATABASE_URL")!;
+
+    // Parse connection URL to connection string for Npgsql
+    connUrl = connUrl.Replace("postgres://", String.Empty);
+    var pgUserPass = connUrl.Split("@")[0];
+    var pgHostPortDb = connUrl.Split("@")[1];
+    var pgHostPort = pgHostPortDb.Split("/")[0];
+    var pgDb = pgHostPortDb.Split("/")[1];
+    var pgUser = pgUserPass.Split(":")[0];
+    var pgPass = pgUserPass.Split(":")[1];
+    var pgHost = pgHostPort.Split(":")[0];
+    var pgPort = pgHostPort.Split(":")[1];
+
+    connString = $"Server={pgHost};Port={pgPort};User Id={pgUser};Password={pgPass};Database={pgDb};";
+}
+builder.Services.AddDbContext<DataContext>(opt => { opt.UseNpgsql(connString); });
+
 var app = builder.Build();
 
 app.UseMiddleware<ExceptionMiddleware>();
 // Configure the HTTP request pipeline.
- if (app.Environment.IsDevelopment())
- {
-     //app.UseDeveloperExceptionPage();
-     app.UseSwagger();
-     app.UseSwaggerUI();
- }
+if (app.Environment.IsDevelopment())
+{
+    //app.UseDeveloperExceptionPage();
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
 app.UseCors(builder => builder
-     .AllowAnyHeader()
-     .AllowAnyMethod()
-     .AllowCredentials()
-     .WithOrigins("https://localhost:4200"));
- 
+    .AllowAnyHeader()
+    .AllowAnyMethod()
+    .AllowCredentials()
+    .WithOrigins("https://localhost:4200"));
+
 app.UseHttpsRedirection();
 app.UseAuthentication();
 app.UseAuthorization();
@@ -65,22 +85,22 @@ app.MapControllers();
 app.MapHub<PresenceHub>("hubs/presence");
 app.MapHub<MessageHub>("hubs/message");
 app.MapFallbackToController("Index", "Fallback");
-using var scope    = app.Services.CreateScope();
-var       services = scope.ServiceProvider;
+using var scope = app.Services.CreateScope();
+var services = scope.ServiceProvider;
 try
 {
-     var logger = services.GetService<ILogger<Seed>>();
-     var context = services.GetRequiredService<DataContext>();
-     var userManager = services.GetRequiredService<UserManager<AppUser>>();
-     var roleManager = services.GetRequiredService<RoleManager<AppRole>>();
-     await context.Database.MigrateAsync();
-     await Seed.ClearConnections(context);
-     await Seed.SeedUsers(logger, userManager, roleManager);
+    var logger = services.GetService<ILogger<Seed>>();
+    var context = services.GetRequiredService<DataContext>();
+    var userManager = services.GetRequiredService<UserManager<AppUser>>();
+    var roleManager = services.GetRequiredService<RoleManager<AppRole>>();
+    await context.Database.MigrateAsync();
+    await Seed.ClearConnections(context);
+    await Seed.SeedUsers(logger, userManager, roleManager);
 }
 catch (Exception e)
 {
-     var logger = services.GetService<ILogger<Program>>();
-     logger.LogError(e, "An error occured during migrations");
+    var logger = services.GetService<ILogger<Program>>();
+    logger.LogError(e, "An error occured during migrations");
 }
 
 app.Run();
